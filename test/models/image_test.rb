77b28a8562a269c657c97f63ea36c1ae1c401760
Devangi Vadhdiya <dvadhdiya0151@conestogac.on.ca>
2021-09-22 03:35:43 +0530

    Test case tried

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -4,4 +4,13 @@ class ImageTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  # test "should not be able to see private image" do
+  #   user1 = User.new(email: "user1@example.com", password: "password", password_confirmation: "password")
+  #   user1.save
+  #   image1 = user1.images.build(is_public: false)
+  #   image1.source.attach(io: File.open('./some.jpeg'), filename: 'some.jpeg')
+  #   image1.save
+  #   assert_not_includes( Image.public_images, image1, "Private image not visible" )
+  # end
 end
